593920a91c92cf7ed111859ae86d3cce7368cc99
Michael Witrant <mike@lepton.fr>
2013-02-17 14:01:53 +0100

    only admin can manage all

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,10 @@ class Ability
   def initialize(user)
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
-    
-    can :manage, :all
+    if current_user.has_role? :admin
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
 end
