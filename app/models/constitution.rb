47bcad2067f97f91854ac2f288476111cd39c04d
Michael Witrant <mike@lepton.fr>
2013-04-21 20:12:02 +0200

    clean text

--- app/models/constitution.rb
+++ app/models/constitution.rb
@@ -2,4 +2,11 @@ class Constitution < ActiveRecord::Base
   attr_accessible :text, :title, :base_id
   
   belongs_to :base, class_name: "Constitution"
+  
+  after_validation :clean_text
+  
+  
+  def clean_text
+    self.text = self.text.gsub("\r\n", "\n") if self.text
+  end
 end
