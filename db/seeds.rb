bbdd029f71b594fc986fa1051c2ed3dad4b38a2a
Michael Witrant <mike@lepton.fr>
2013-04-21 18:59:22 +0200

    seed law text

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+french_constitution = LawText.where(title: "Constitution française").first_or_initialize
+french_constitution.text = Rails.root.join('db', 'french_constitution.txt').read
+french_constitution.save!
