75e765d68d333bbc496a5648f656a61de9147511
Michael Witrant <mike@lepton.fr>
2013-04-28 17:13:54 +0200

    better user page

--- app/models/constitution.rb
+++ app/models/constitution.rb
@@ -2,7 +2,7 @@ class Constitution < ActiveRecord::Base
   attr_accessible :text, :title, :base_id
   
   belongs_to :base, class_name: "Constitution"
-  belongs_to :user
+  belongs_to :user, inverse_of: :constitutions
   
   after_validation :clean_text
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   
+  has_many :constitutions, inverse_of: :user
+  
   def display_name
     name.presence || I18n.t(:anonmyous)
   end
