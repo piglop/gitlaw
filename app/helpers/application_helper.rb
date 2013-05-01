ea7e19f5a2eab7f49c0a3407101301172cfcfcb1
Michael Witrant <mike@lepton.fr>
2013-05-01 11:23:14 +0200

    text index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,23 @@
 module ApplicationHelper
-  def link_to_text(text, target = text, *options)
-    user = text.user
+  def link_to_text(text, *target_and_options)
+    options = target_and_options.extract_options!
+    target = target_and_options[0] || text
+    
     capture_haml do
-      if user
-        haml_tag("span.user", link_to(user.display_name, user))
-        haml_tag("span.separator", "/")
+      unless options[:without_user]
+        user = text.user
+        if user
+          haml_tag("span.user", link_to(user.display_name, user))
+          haml_tag("span.separator", "/")
+        end
       end
-      haml_tag("span.text", link_to(text.title, target, *options))
+      haml_tag("span.text", link_to(text.title, target, options))
+    end
+  end
+  
+  def link_to_user(user)
+    if user
+      link_to user.display_name, user
     end
   end
 end
